4a7fb9832f6c4db91261045cfbfe90ef8d7679d6
wang xiao <673179458@qq.com>
2018-03-01 12:08:00 +0800

    update rpc

--- rpc/ipsec.go
+++ rpc/ipsec.go
@@ -2,28 +2,21 @@ package client
 
 import (
 	"log"
+	clientrpc "script/ipsecMonitor/client/rpc"
 )
 
 type IpsecRpcClient struct {
 	RpcClient
 }
 
-type Status struct {
-	Succ bool
-}
-
-type Input struct {
-	Stamp int64
-}
-
 func (this *IpsecRpcClient) Connect(host string, port int) bool {
 	return this.ConnectRemote(host, port)
 }
 
-func (this *IpsecRpcClient) RestartIpsec(stamp int64) *Status {
-	replay := Status{}
-	req := Input{Stamp: stamp}
-	err := this.Handle.Call("Handler.RestartIpsec", &req, &replay)
+func (this *IpsecRpcClient) RestartIpsec(stamp int64) *clientrpc.Status {
+	replay := clientrpc.Status{}
+	args := clientrpc.Args{Stamp: stamp}
+	err := this.Handle.Call("Handler.RestartIpsec", &args, &replay)
 	if err != nil {
 		log.Println(err)
 		return nil
